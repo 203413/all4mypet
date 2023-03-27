31df0d9a4835cb431d08a16f3329729a2e8b4a01
AvrlFstr <77992655+203413@users.noreply.github.com>
2023-03-26 20:21:54 -0600

    minor fixex

--- all4mypet/settings.py
+++ all4mypet/settings.py
@@ -84,16 +84,16 @@ WSGI_APPLICATION = 'all4mypet.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'all4mypet',
-        'USER': 'postgres',
-        'PASSWORD': 'greggrulzok',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'all4mypet',
+#         'USER': 'postgres',
+#         'PASSWORD': 'greggrulzok',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
